d4bebe0012d049432ad30c50c3e6cd4cb8e9fa68
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-04-14 13:09:34 +0300

    Add initial project

---
+++ src/main/java/bg/fmi/rateuni/RateUniApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.rateuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateUniApplication.class, args);
+	}
+
+}
