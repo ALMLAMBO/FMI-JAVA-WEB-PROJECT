97d96de794758980980661d6d60d2d445cfc59c7
A-KeranoF <naskokeranov@abv.bg>
2024-04-21 15:10:08 +0300

    Add model for Faculty

---
+++ src/main/java/bg/fmi/rateuni/models/base/Faculty.java
@@ -0,0 +1,12 @@
+package bg.fmi.rateuni.models.base;
+
+import lombok.Data;
+
+@Data
+public class Faculty {
+    private int id;
+    private String name;
+    private String dean;
+    private String address;
+
+}
