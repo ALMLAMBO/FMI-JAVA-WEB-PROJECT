0acae29ea625fd2b7e83752bc6b10e61125be2f0
A-KeranoF <naskokeranov@abv.bg>
2024-05-14 20:11:33 +0300

    Added DB annotations for Discipline class and auto DDL update

--- src/main/java/bg/fmi/rateuni/models/base/Discipline.java
+++ src/main/java/bg/fmi/rateuni/models/base/Discipline.java
@@ -2,15 +2,30 @@ package bg.fmi.rateuni.models.base;
 
 import bg.fmi.rateuni.vo.DisciplineCategory;
 import bg.fmi.rateuni.vo.DisciplineType;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "disciplines")
 public class Discipline {
-    private int id;
+    @Id
+    private UUID id;
     private String name;
     private String description;
     private double credits;
+
+    @Enumerated(EnumType.STRING)
     private DisciplineCategory category; // ЯКН, ОКН, Мат...
+
+    @Enumerated(EnumType.STRING)
     private DisciplineType type;
     private String lecturer;
     private String assistants;
