2394bcbd82eba9f7493ed9a6b0aecaa2213efe23
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-05-20 21:47:30 +0300

    Create CRUD service for role

---
+++ src/main/java/bg/fmi/rateuni/services/crud/RoleService.java
@@ -0,0 +1,28 @@
+package bg.fmi.rateuni.services.crud;
+
+import bg.fmi.rateuni.models.Role;
+import bg.fmi.rateuni.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+    
+    public Optional<Role> getRoleById(UUID id) {
+        return roleRepository.findById(id);
+    }
+    
+    public void createRole(Role role) {
+        roleRepository.save(role);
+    }
+}
