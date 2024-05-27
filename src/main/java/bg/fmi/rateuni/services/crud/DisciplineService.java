e3b002c1428072762d9082892844355e73f32958
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-05-27 19:48:05 +0300

    Add custom methods for adding and fetching data for the discipline

--- src/main/java/bg/fmi/rateuni/services/crud/DisciplineService.java
+++ src/main/java/bg/fmi/rateuni/services/crud/DisciplineService.java
@@ -1,12 +1,15 @@
 package bg.fmi.rateuni.services.crud;
 
 import bg.fmi.rateuni.models.Discipline;
+import bg.fmi.rateuni.models.Programme;
+import bg.fmi.rateuni.models.Review;
 import bg.fmi.rateuni.repository.DisciplineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -24,5 +27,29 @@ public class DisciplineService {
 
     public void createUpdateDiscipline (Discipline discipline) {
         disciplineRepository.save(discipline);
+    }
+    
+    public void addReviewToDiscipline(UUID disciplineId, Set<Review> reviews) {
+        disciplineRepository.addReviewToDiscipline(disciplineId, reviews);
+    }
+
+    public List<Review> getDisciplineReviews(UUID id) {
+        return disciplineRepository.findDisciplineReviewsById(id);
+    }
+    
+    public void addDisciplineToProgramme(UUID disciplineId, Set<Programme> programmes) {
+        disciplineRepository.addDisciplineToProgramme(disciplineId, programmes);
+    }
+    
+    public List<Programme> getDisciplineProgrammes(UUID id) {
+        return disciplineRepository.findDisciplineProgramsById(id);
+    }
+    
+    public void addUserToDiscipline(UUID disciplineId, Set<Programme> users) {
+        disciplineRepository.addUserToDiscipline(disciplineId, users);
+    }
+    
+    public List<Programme> getDisciplineUsers(UUID id) {
+        return disciplineRepository.findDisciplineUsersById(id);
     }
 }
