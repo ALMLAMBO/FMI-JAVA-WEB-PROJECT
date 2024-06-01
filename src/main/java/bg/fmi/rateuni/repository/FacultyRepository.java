7ef3ec5e7239d1528ec8bc4d2fae369b17cc3c28
A-KeranoF <naskokeranov@abv.bg>
2024-06-01 13:11:32 +0300

    Added CRUD operations mainly in the Faculty and Programme repositories.

--- src/main/java/bg/fmi/rateuni/repository/FacultyRepository.java
+++ src/main/java/bg/fmi/rateuni/repository/FacultyRepository.java
@@ -1,11 +1,40 @@
 package bg.fmi.rateuni.repository;
 
-import bg.fmi.rateuni.models.Faculty;
+import bg.fmi.rateuni.models.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface FacultyRepository extends JpaRepository<Faculty, UUID> {
+
+    @Modifying
+    @Query("UPDATE Faculty f SET f.facultyPrograms = :facultyPrograms WHERE f.id = :facultyId")
+    void addProgrammeToFaculty(@Param("facultyId") UUID facultyId,
+                               @Param("facultyPrograms") List<Programme> facultyPrograms);
+
+    @Query("SELECT f.facultyPrograms FROM Faculty f WHERE f.id = :id")
+    List<Programme> findProgramsByFacultyId(@Param("id") UUID id);
+
+    @Modifying
+    @Query("UPDATE University u SET u.universityFaculties = :universityFaculties WHERE u.id = :universityId")
+    void addFacultyToUniversity(@Param("universityId") UUID universityId,
+                                @Param("universityFaculties") List<Faculty> faculties);
+
+    @Query("SELECT u.universityFaculties FROM University u WHERE u.id = :id")
+    List<Faculty> findFacultiesByUniversityId(@Param("id") UUID id);
+
+    @Modifying
+    @Query("UPDATE Faculty f SET f.users = :users WHERE f.id = :facultyId")
+    void addUserToFacultyBy(@Param("facultyId") UUID facultyId,
+                            @Param("users") List<User> users);
+
+    @Query("SELECT f.users FROM Faculty f WHERE f.id =:id")
+    List<User> findUsersByFacultyId(@Param("id") UUID id);
 }
--- src/main/java/bg/fmi/rateuni/repository/ProgrammeRepository.java
+++ src/main/java/bg/fmi/rateuni/repository/ProgrammeRepository.java
@@ -1,11 +1,24 @@
 package bg.fmi.rateuni.repository;
 
 import bg.fmi.rateuni.models.Programme;
+import bg.fmi.rateuni.models.Review;
+import bg.fmi.rateuni.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface ProgrammeRepository extends JpaRepository<Programme, UUID> {
+    @Modifying
+    @Query("UPDATE Programme p SET p.users = :users WHERE p.id = :programmeId")
+    void addUserToProgramme(@Param("programmeId") UUID programmeId, @Param("users") List<User> users);
+
+    @Query("SELECT p.users FROM Programme p WHERE p.id = :programmeId")
+    List<User> findAllUsersByProgrammeId(@Param("programmeId") UUID programmeId);
 }
