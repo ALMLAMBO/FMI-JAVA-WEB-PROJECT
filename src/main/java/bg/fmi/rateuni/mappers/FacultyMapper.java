4613ed241ba00848b327cfb1f4a3a71e61bbdb03
A-KeranoF <naskokeranov@abv.bg>
2024-06-05 21:34:35 +0300

    Updated FaciltyMapper and ProgrammeMapper mappings

--- src/main/java/bg/fmi/rateuni/mappers/FacultyMapper.java
+++ src/main/java/bg/fmi/rateuni/mappers/FacultyMapper.java
@@ -5,19 +5,16 @@ import bg.fmi.rateuni.dto.response.FacultyResponse;
 import bg.fmi.rateuni.models.Faculty;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface FacultyMapper {
-    @Mapping(source = "idResponse", target = "id")
+    @Mapping(source = "idResponse", target = "faculty.id")
     Faculty mapFromDto(FacultyResponse facultyDto);
 
-    @Mapping(source = "id", target = "idInfoResponse")
+    @Mapping(source = "faculty.id", target = "idInfoResponse")
     @Mapping(source = "facultyPrograms", target = "programs")
     FacultyInfoResponse mapToInfoResponseDto(Faculty faculty);
 
-    @Mapping(source = "id", target = "idResponse")
+    @Mapping(source = "faculty.id", target = "idResponse")
     FacultyResponse mapToDto(Faculty faculty);
 }
--- src/main/java/bg/fmi/rateuni/mappers/ProgrammeMapper.java
+++ src/main/java/bg/fmi/rateuni/mappers/ProgrammeMapper.java
@@ -5,17 +5,19 @@ import bg.fmi.rateuni.dto.response.ProgrammeResponse;
 import bg.fmi.rateuni.models.Programme;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface ProgrammeMapper {
-    @Mapping(source = "idResponse", target = "id")
-    Programme mapFromDto(ProgrammeResponse programmeDto);
 
-    @Mapping(source = "id", target = "idInfoResponse")
+    @Mapping(source = "programmeDto.idInfoResponse", target = "id")
+    @Mapping(source = "programmeDto.disciplines", target = "programmeDisciplines")
+    Programme mapFromDto(ProgrammeResponse programmeDto);
+    
+    @Mapping(source = "programme.id", target = "idInfoResponse")
     @Mapping(source = "programmeDisciplines", target = "disciplines")
     ProgrammeInfoResponse mapToInfoResponseDto(Programme programme);
-
-    @Mapping(source = "id", target = "idResponse")
+    
+    @Mapping(source = "programme.id", target = "idResponse")
+    @Mapping(source = "programmeDisciplines", target = "disciplines")
     ProgrammeResponse mapToDto(Programme programme);
 }
