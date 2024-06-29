3e1c76fa22af37e86659545a6e346dded05a718e
A-KeranoF <91877625+A-KeranoF@users.noreply.github.com>
2024-06-29 22:53:50 +0300

    Added UserController

---
+++ src/main/java/bg/fmi/rateuni/controllers/UserController.java
@@ -0,0 +1,38 @@
+package bg.fmi.rateuni.controllers;
+
+import bg.fmi.rateuni.dto.request.RegisterRequest;
+import bg.fmi.rateuni.dto.response.BaseResponse;
+import bg.fmi.rateuni.dto.response.UserDetail;
+import bg.fmi.rateuni.services.business.AuthService;
+import bg.fmi.rateuni.services.business.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDetail> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getUserDetails(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> createUser(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.registerUser(registerRequest));
+    }
+
+    @PostMapping("/{id}/discipline")
+    public ResponseEntity<BaseResponse> addDisciplineToUser(@PathVariable UUID disciplineId, @RequestBody UserDetail userDetail) {
+        return ResponseEntity.ok(userService.addUserToDiscipline(userDetail, disciplineId));
+    }
+}
