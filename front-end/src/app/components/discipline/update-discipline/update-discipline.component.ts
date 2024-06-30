83ce5ebf4630a9d84f01463a6be9a3a339d875e3
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-30 22:03:48 +0300

    Generate components

---
+++ front-end/src/app/components/discipline/update-discipline/update-discipline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-discipline',
+  templateUrl: './update-discipline.component.html',
+  styleUrl: './update-discipline.component.scss'
+})
+export class UpdateDisciplineComponent {
+
+}
---
+++ front-end/src/app/components/programme/create-programme/create-programme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-programme',
+  templateUrl: './create-programme.component.html',
+  styleUrl: './create-programme.component.scss'
+})
+export class CreateProgrammeComponent {
+
+}
