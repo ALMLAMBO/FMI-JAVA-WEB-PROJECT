3218fcb79503e63a1ec4ee025b8648ad21abc8fe
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-30 20:06:12 +0300

    Fix user controller and configure angular with scss

--- src/main/java/bg/fmi/rateuni/controllers/UserController.java
+++ src/main/java/bg/fmi/rateuni/controllers/UserController.java
@@ -1,5 +1,6 @@
 package bg.fmi.rateuni.controllers;
 
+import bg.fmi.rateuni.dto.request.LoginRequest;
 import bg.fmi.rateuni.dto.request.RegisterRequest;
 import bg.fmi.rateuni.dto.response.BaseResponse;
 import bg.fmi.rateuni.dto.response.UserDetail;
@@ -7,13 +8,12 @@ import bg.fmi.rateuni.services.business.AuthService;
 import bg.fmi.rateuni.services.business.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -26,11 +26,16 @@ public class UserController {
         return ResponseEntity.ok(userService.getUserDetails(id));
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<BaseResponse> createUser(@RequestBody RegisterRequest registerRequest) {
         return ResponseEntity.ok(authService.registerUser(registerRequest));
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.loginUser(loginRequest));
+    }
+    
     @PostMapping("/{id}/discipline")
     public ResponseEntity<BaseResponse> addDisciplineToUser(@PathVariable UUID id, @RequestBody UserDetail userDetail) {
         return ResponseEntity.ok(userService.addUserToDiscipline(userDetail, id));
