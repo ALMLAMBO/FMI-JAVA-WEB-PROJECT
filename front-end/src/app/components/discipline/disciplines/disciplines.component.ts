55ccf339c20a85c7caff6ee50e5004647cf841f4
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-07-01 15:17:37 +0300

    Finish disciplines component

--- front-end/src/app/components/discipline/disciplines/disciplines.component.ts
+++ front-end/src/app/components/discipline/disciplines/disciplines.component.ts
@@ -1,12 +1,28 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { DisciplineResponse } from '../../../dto/response/discipline-response';
+import {ActivatedRoute} from "@angular/router";
+import {environment} from "../../../../environments/environment.development";
 
 @Component({
   selector: 'app-disciplines',
   templateUrl: './disciplines.component.html',
   styleUrl: './disciplines.component.scss'
 })
-export class DisciplinesComponent {
-  @Input() disciplines!: DisciplineResponse[];
+export class DisciplinesComponent implements OnInit {
+  @Input() disciplines: DisciplineResponse[] = [];
+  universityId: string = '';
+  facultyId: string = '';
+  programmeId: string = '';
 
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.universityId = params[environment.urlIds.university];
+      this.facultyId = params[environment.urlIds.faculty];
+      this.programmeId = params[environment.urlIds.programme];
+    });
+  }
 }
