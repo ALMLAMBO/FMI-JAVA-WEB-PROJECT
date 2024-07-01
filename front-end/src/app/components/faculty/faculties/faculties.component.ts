6bf7fefe8938a0edcd5108edcb5ea5646414ce65
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-07-01 11:03:00 +0300

    Finish faculties component

--- front-end/src/app/components/faculty/faculties/faculties.component.ts
+++ front-end/src/app/components/faculty/faculties/faculties.component.ts
@@ -1,12 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FacultyResponse} from "../../../dto/response/faculty-response";
+import {ActivatedRoute} from "@angular/router";
+import {environment} from "../../../../environments/environment.development";
 
 @Component({
   selector: 'app-faculties',
   templateUrl: './faculties.component.html',
   styleUrl: './faculties.component.scss'
 })
-export class FacultiesComponent {
-  @Input() faculties!: FacultyResponse[];
+export class FacultiesComponent implements OnInit {
+  @Input() faculties: FacultyResponse[];
 
+  universityId: string = '';
+
+  constructor(private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.universityId = params[environment.urlIds.university];
+    });
+  }
 }
